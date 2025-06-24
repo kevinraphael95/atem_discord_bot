9640a20f180ea3dea6fc9052cadd8d4f09554df1
Kévin <54324251+kevinraphael95@users.noreply.github.com>
2025-06-24 18:32:52 +0200

    Update testquestion.py

--- commands/ygo/testquestion.py
+++ commands/ygo/testquestion.py
@@ -274,6 +274,8 @@ class TestQuestion(commands.Cog):
 
 
             true_card = main_card
+            # ✅ Filtrage de carte banned word pur les fausses réponses
+            group = [c for c in group if is_clean_card(c)]
             wrongs = random.sample(group, 3)
             all_choices = [true_card["name"]] + [c["name"] for c in wrongs]
             random.shuffle(all_choices)
