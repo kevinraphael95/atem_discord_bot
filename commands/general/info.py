e060620b174a594cc94ac5ef0625e04cbbd64421
Kévin <54324251+kevinraphael95@users.noreply.github.com>
2025-06-26 02:32:52 +0200

    Update info.py

--- commands/general/info.py
+++ commands/general/info.py
@@ -42,7 +42,8 @@ class InfoCog(commands.Cog):
             name="🃏 Yu-Gi-Oh!",
             value=(
                 "\n"
-                "D'abord les commandes sur Yu-Gi-Oh en général, y'a surtout les deux minijeux question et illustration, et la commande carte pour voir les infos d'une carte YGO.\n"
+                "D'abord les commandes sur Yu-Gi-Oh en général, y'a surtout les deux minijeux question et illustration, "
+                "et la commande carte pour voir les infos d'une carte YGO.\n\n"
                 "**• Question :** Devinez la carte avec sa description.\n"
                 "( Y'a aussi TestQuestion, une tentative d'ammélioration de la commande question, test. )\n"
                 "**• Illustration :** Devinez la carte avec son illustration.\n"
@@ -55,7 +56,8 @@ class InfoCog(commands.Cog):
         embed.add_field(
             name="🎮 VAACT",
             value=(
-                "Et les commandes pour le tournoi Yu-Gi-Oh VAACT pour voir quand est le prochain (si y'en a un de prévu) et les decks personnalisés manga/anime du tournoi."
+                "Et les commandes pour le tournoi Yu-Gi-Oh VAACT pour voir quand est le prochain (si y'en a un de prévu) "
+                "et les decks personnalisés manga/anime du tournoi.\n\n"
                 "**• Tournoi :** Regardez si un tournoi VAACT est prévu et si oui sa date.\n"
                 "**• Deck :** Regardez les decks custom du tournoi version animé/manga (y'aura des astuces avec peut-être un jour)"
             ),
