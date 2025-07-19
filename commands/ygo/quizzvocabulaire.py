7952d8399f077a5a645d882c2ba7f2529d1b9a4a
Kévin <54324251+kevinraphael95@users.noreply.github.com>
2025-07-19 15:41:49 +0200

    Update quizzvocabulaire.py

--- commands/ygo/quizzvocabulaire.py
+++ commands/ygo/quizzvocabulaire.py
@@ -115,5 +115,5 @@ async def setup(bot: commands.Bot):
     cog = QuizzVocabulaire(bot)
     for command in cog.get_commands():
         if not hasattr(command, "category"):
-            command.category = "Yu-Gi-Oh"
+            command.category = "🎴 Yu-Gi-Oh"
     await bot.add_cog(cog)
