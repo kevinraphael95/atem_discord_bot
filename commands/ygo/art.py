31ebbc696ff5895dc9efa2187228086314dd18d5
Kévin <54324251+kevinraphael95@users.noreply.github.com>
2025-09-09 04:04:55 +0200

    Update art.py

--- commands/ygo/art.py
+++ commands/ygo/art.py
@@ -138,5 +138,5 @@ async def setup(bot: commands.Bot):
     cog = ArtCommand(bot)
     for command in cog.get_commands():
         if not hasattr(command, "category"):
-            command.category = "Yu-Gi-Oh!"
+            command.category = "🃏 Yu-Gi-Oh!"
     await bot.add_cog(cog)
