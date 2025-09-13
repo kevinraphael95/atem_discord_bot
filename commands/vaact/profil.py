2c8bad80f1525821e5f26ff024bedcc977453b2a
Kévin <54324251+kevinraphael95@users.noreply.github.com>
2025-09-13 04:35:04 +0200

    Update profil.py

--- commands/vaact/profil.py
+++ commands/vaact/profil.py
@@ -98,5 +98,4 @@ async def setup(bot: commands.Bot):
     for command in cog.get_commands():
         if not hasattr(command, "category"):
             command.category = "Profil"
-    await bot.ad
-    d_cog(cog)
+    await bot.add_cog(cog)
